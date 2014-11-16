83ee52827b692cdba212c3abf384494dca369007
523860169 <pigoig@gmail.com>
2014-11-15 22:20:20 -0500

    update

---
+++ addons/zh-CN/firefox/addons/buttons.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+z.button.messages = {
+  learn_more: "<div class=\"install-note\">\n      <p class=\"msg m-learn-more\">\u8981\u5b89\u88c5\u8fd9\u4e2a\u6216\u5176\u4ed6\u6570\u4ee5\u5343\u8ba1\u7684\u9644\u52a0\u7ec4\u4ef6\uff0c<b>\u4e0b\u8f7d Firefox</b>\uff0cMozilla \u51fa\u54c1\u7684\u514d\u8d39\u3001\u5f00\u653e\u7684\u7f51\u9875\u6d4f\u89c8\u5668\u3002</p>\n    <p><a href=\"http://getfirefox.com/?src=amo\" class=\"button\">\u8fdb\u4e00\u6b65\u4e86\u89e3 Firefox</a></p>\n    <p>\u6216\u8005 <b><a class=\"installer\" href=\"{url}\">\u575a\u6301\u4e0b\u8f7d</a></b></p>\n  </div>\n",
+  learn_more_personas: "<div class=\"install-note\">\n      <p class=\"msg m-learn-more\">\u8981\u5b89\u88c5\u8fd9\u4e2a\u6216\u8005\u6570\u4ee5\u5343\u8ba1\u7684\u66f4\u591a\u4e3b\u9898\uff0c<b>\u4e0b\u8f7d Firefox</b>\uff0cMozilla \u51fa\u54c1\u7684\u514d\u8d39\u5f00\u653e\u7684\u7f51\u9875\u6d4f\u89c8\u5668\u3002</p>\n    <p><a href=\"http://getfirefox.com/?src=amo\" class=\"button\">\u8fdb\u4e00\u6b65\u4e86\u89e3 Firefox</a></p>\n  </div>\n",
+  not_updated: "<div class=\"install-note\">\n  <p class=\"msg m-not-updated\">\u8fd9\u4e2a\u9644\u52a0\u7ec4\u4ef6\u5c1a\u672a\u66f4\u65b0\u4ee5\u517c\u5bb9\u60a8\u7684 Firefox \u7248\u672c\u3002</p>\n  <p><a href=\"{url}\" class=\"button installer\">\u575a\u6301\u5b89\u88c5</a></p>\n</div>\n",
+  newer_version: "<div class=\"install-note extension\">\n  <div class=\"beta msg\">\n    <p class=\"m-newer-version\">\u6b64\u9644\u52a0\u7ec4\u4ef6\u9700\u8981\u4e00\u4e2a\u5c1a\u672a\u53d1\u5e03\u7684 Firefox \u7248\u672c\u3002</p>\n    <p><a href=\"http://www.mozilla.com/firefox/beta/\" class=\"button\">\u5e2e\u5fd9\u6d4b\u8bd5\u65b0\u7684 Firefox \u7248\u672c</a></p>\n  </div>\n  <div class=\"nobeta msg\">\n    <p class=\"m-newer-version\">\u6b64\u9644\u52a0\u7ec4\u4ef6\u9700\u8981 Firefox {new_version}\uff0c\u60a8\u76ee\u524d\u4f7f\u7528\u7684\u662f Firefox {old_version}\u3002</p>\n        <p><a href=\"http://getfirefox.com/?src=amo\" class=\"button\">\u5347\u7ea7 Firefox</a></p>\n  </div>\n    <p class=\"older-versions\">\u6216\u8005\u67e5\u770b<a href=\"/zh-CN/firefox/addons/versions/{addon}\">\u6b64\u9644\u52a0\u7ec4\u4ef6\u7684\u65e9\u671f\u7248\u672c</a>\u3002</p>\n</div>\n",
+  personas_need_upgrade: "<div class=\"install-note\">\n    <p class=\"msg m-newer-version\">\u6b64\u70ab\u5f69\u98ce\u683c\u9700\u8981 Firefox 3.6\uff0c\u60a8\u76ee\u524d\u4f7f\u7528\u7684\u662f Firefox {old_version}\u3002</p>\n    <p><a href=\"http://getfirefox.com/?src=amo\" class=\"button\">\u5347\u7ea7 Firefox</a></p>\n</div>\n",
+  unreviewed: "<div class=\"install-note\">\n  <p class=\"msg m-unreviewed\"><strong>\u6ce8\u610f\uff1a</strong> \u8be5\u9644\u52a0\u7ec4\u4ef6\u5c1a\u672a\u7ecf\u8fc7 Mozilla \u5ba1\u6838\u3002\u5b89\u88c5\u7b2c\u4e09\u65b9\u8f6f\u4ef6\u53ef\u80fd\u4f1a\u635f\u5bb3\u60a8\u7684\u8ba1\u7b97\u673a\u3002</p>\n  <p><a href=\"{url}\" class=\"button installer\">{msg}</a></p>\n</div>\n",
+  platform_link: "  <li><a class=\"installer\" href=\"{href}\">{msg}</a></li>\n",
+  bad_platform: "<div class=\"install-note\">\n  <p class=\"msg m-platforms\"><strong>\u8bf7\u6ce8\u610f\uff1a</strong>\u6b64\u9644\u52a0\u7ec4\u4ef6\u4e0e\u60a8\u7684\u64cd\u4f5c\u7cfb\u7edf\u4e0d\u517c\u5bb9\u3002</p>\n  <ul>{platforms}</ul>\n</div>\n",
+  not_compatible: "<div class=\"install-note\">\n  <p class=\"msg m-not-compatible\">\u56e0\u4e3a\u4ee5\u4e0b\u539f\u56e0\uff0c\u6b64\u9644\u52a0\u7ec4\u4ef6\u4e0e\u60a8\u7684 Firefox \u7248\u672c\u4e0d\u517c\u5bb9\uff1a</p>\n  <ul>{reasons}</ul>\n  <p><a href=\"{url}\" class=\"button installer\">\u575a\u6301\u5b89\u88c5</a></p>\n  <p><a href=\"{versions_url}\">\u67e5\u770b\u5176\u4ed6\u7248\u672c</a></p>\n</div>\n",
+  not_compatible_no_reasons: "<div class=\"install-note\">\n  <p class=\"msg m-not-compatible\">\u6b64\u9644\u52a0\u7ec4\u4ef6\u4e0e\u60a8\u7684 Firefox \u7684\u7248\u672c\u4e0d\u517c\u5bb9\u3002</p>\n  <p><a href=\"{url}\" class=\"button installer\">\u575a\u6301\u5b89\u88c5</a></p>\n  <p><a href=\"{versions_url}\">\u67e5\u770b\u5176\u4ed6\u7248\u672c</a></p>\n</div>\n"
+};
