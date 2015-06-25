e3c6bb9a25ec911e485827ed211c2dace844b0c3
523860169 <pigoig@gmail.com>
2015-06-25 01:20:16 -0400

    update

--- js/data.js
+++ js/data.js
@@ -4,13 +4,12 @@ var Config = getMStr(function(){
 /*
 资讯
 	IT 资讯,  http://www.ithome.com/list/, img/web/ithome.ico
-	抽屉新热榜,  http://dig.chouti.com/, img/web/chouti.ico
 	CNBeta,     http://www.cnbeta.com/, img/web/cnBeta.ico
 	果壳网,   http://www.guokr.com/, img/web/guokr.ico
 	淘宝网, http://www.taobao.com/, img/web/taobao.ico
-
+	什么值得买, http://www.smzdm.com/, img/web/smzdm.png
 社区
-	ZZIA吧, http://tieba.baidu.com/f?ie=utf-8&kw=zzia, img/web/tieba.png
+	抽屉新热榜,  http://dig.chouti.com/, img/web/chouti.ico
 	Firefox吧, http://tieba.baidu.com/f?ie=utf-8&kw=firefox, img/web/tieba.png
 	HalfLife吧, http://tieba.baidu.com/f?ie=utf-8&kw=halflife, img/web/tieba.png
 	卡饭, http://bbs.kafan.cn/forum-215-1.html, img/web/kafan.ico
@@ -21,14 +20,12 @@ var Config = getMStr(function(){
 	吐槽网, http://www.tucao.cc/, img/web/tucao.ico
 	嘀哩嘀哩, http://www.dilidili.com/, img/web/dilidili.ico
 	52天, http://www.52tian.net/, img/web/52tian.ico
-
 资源
 	ZD423, http://www.zdfans.com/, img/web/zd423.ico
 	ED2000, http://www.ed2000.com/, img/web/ed2000.ico
 	迅播, http://www.2tu.cc/index.html, img/web/2tu.ico
 	BT天堂, http://www.bttiantang.com/, img/web/bttiantang.ico
 	电影天堂, http://www.dy2018.com/, img/web/dy2018.ico
-
 应用
 	Bing翻译, http://www.bing.com/translator/, img/web/translate.png
 	百度地图, http://map.baidu.com/, img/web/gmaps.ico
@@ -37,7 +34,7 @@ var Config = getMStr(function(){
 	BookLink, http://booklink.me/, img/web/booklink.ico
 综合
 	百度首页, http://www.baidu.com/, img/web/baidu.ico
-	分站, http://www.minggo.tk, img/web/minggo.ico
+	分站, http://www.minggo.tk/, img/web/minggo.ico
 	搜图标, http://www.easyicon.net/, img/web/easyicon.png
 	酷安网, http://www.coolapk.com/, img/web/coolapk.ico
 	网易云音乐, http://music.163.com/, img/web/music.ico
